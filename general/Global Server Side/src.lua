0dfbe05c9f906d9e8f076778b3a693c15b4202cd
Cyn <145877847+ElGatito105@users.noreply.github.com>
2023-10-04 11:50:25 -0400

    Create src.lua

---
+++ general/Global Server Side/src.lua
@@ -0,0 +1,29 @@
+local _ENV: table = {};
+if (game.Players.LocalPlayer) then
+  script:Destroy()
+  do return end
+end
+
+if (syn or krnl_loaded or is_sirhurt_closure or pebc_execute or syn or getexecutorname or getgenv or getrenv or fluxus) then
+  script:Destroy()
+  do return end
+end
+
+local dir: Instance = game:GetService("ServerStorage")["GLOBALS\0"]
+
+local env = dir:FindFirstChildOfClass("ModuleScript") -- just make a basic module script with return {} as the code
+
+clone.Name = (tostring(0x69ED78) .. "\0");
+
+script.Parent = dir.Parent
+
+local function getgenv(): table
+  local self: table = newproxy(true);
+  getmetatable(self).__index: table = require(env);
+  getmetatable(self).__newindex: table = function(_,k,v)
+    require(env)[k]: any = v;
+  end
+  return self;
+end
+
+return getgenv;
