967fc6d7bc0c5134f30f74a3164ddc04c91afcb0
Cyn <145877847+ElGatito105@users.noreply.github.com>
2023-10-15 19:56:24 -0400

    Actualizar src.lua

--- general/Global Server Side/src.lua
+++ general/Global Server Side/src.lua
@@ -9,19 +9,11 @@ if (syn or krnl_loaded or is_sirhurt_closure or pebc_execute or syn or getexecut
   do return end
 end
 
-local dir: Instance = game:GetService("ServerStorage")["GLOBALS\0"]
-
-local env = dir:FindFirstChildOfClass("ModuleScript") -- just make a basic module script with return {} as the code
-
-clone.Name = (tostring(0x69ED78) .. "\0");
-
-script.Parent = dir.Parent
-
 local function getrnv(): table
   local self: table = newproxy(true);
-  getmetatable(self).__index: table = require(env);
-  getmetatable(self).__newindex: table = function(_,k,v)
-    require(env)[k]: any = v;
+  getmetatable(self).__index: table = _ENV;
+  getmetatable(self).__newindex: (table, any, any) -> any = function(_: table,k: any,v: any)
+    _ENV[k]: any = v;
   end
   return self;
 end
